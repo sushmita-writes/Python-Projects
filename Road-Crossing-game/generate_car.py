ab221ef041e4775cc08bea2e9087427bad569d5c
Sushmita Shrestha <shresthasush3225@gmail.com>
2025-09-29 19:42:35 +0545

    feat: generate car at random y-pos withiin the window

--- Road-Crossing-game/generate_car.py
+++ Road-Crossing-game/generate_car.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
-from configure import X_MAX, CAR_SPEED
+from configure import X_MAX, CAR_SPEED, Y_MAX
+import random
 
 class Car(Turtle):
 
@@ -9,12 +10,12 @@ class Car(Turtle):
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=1, stretch_len=2)
+        self.y = random.randint(-Y_MAX + 30, Y_MAX - 30)
         self.teleport(x=X_MAX + 10, y=0)
         self.penup()
 
     def move(self):
         x = self.xcor()
-        y = self.ycor()
         # move the car only within the bounds
         if x > -X_MAX - 20:
-            self.goto(x=x - CAR_SPEED, y=y)
+            self.goto(x=x - CAR_SPEED, y=self.y)
