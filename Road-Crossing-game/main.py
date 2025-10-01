41c8d17391bdad1986cd30ee37777df7e0e615f8
Sushmita Shrestha <shresthasush3225@gmail.com>
2025-10-01 08:51:32 +0545

    update: generate car at random times rather than fixed interval

--- Road-Crossing-game/main.py
+++ Road-Crossing-game/main.py
@@ -5,6 +5,7 @@ from road import Road
 from player import Player
 from status_flag import Level, GameOver
 import time
+import random
 
 window = Screen()
 window.setup(width=W_WIDTH, height=W_HEIGHT)
@@ -22,11 +23,15 @@ window.onkeypress(fun=my_player.move_up, key="Up")
 cars = [Car()]
 
 game_end = False
-interval = 1
 
 while not game_end:
     time.sleep(0.1)
     window.update()
+
+    # the new car to generate at random times
+    chance = random.randint(1, 5)
+    if chance == 5:
+        cars.append(Car())
 
     for car in cars:
         car.move(car_speed)
@@ -35,11 +40,6 @@ while not game_end:
             game_over = GameOver()
             game_end = True
             break
-
-    # the new car to generate at certain interval
-    if interval % 5 == 0:
-        cars.append(Car())
-    interval += 1
 
     # if player has crossed the road, level up and return it to start
     if my_player.ycor() > ROAD_EDGE_Y + 10:
